3a4e3ea4c3e5861440439170b9cfb44734a93d40
super-rogatory <chukwudi10@gmail.com>
2021-09-21 13:31:05 -0700

    controversial updates: stopped user routes from sending after headers have been sent. added error handling for chat component so we can redirect if localstorage is missing or query to database is erroneous

--- src/client/components/Chat.js
+++ src/client/components/Chat.js
@@ -5,6 +5,7 @@ import { addMessage, createUser, getUser, getUsers, getUsersInRoom } from '../..
 import ChatHeader from './ChatHeader';
 import Input from './Input';
 import MessageList from './MessageList';
+import { Redirect } from 'react-router-dom';
 
 let clientSocket;
 clientSocket = io('localhost:8080');
@@ -15,8 +16,8 @@ class Chat extends React.Component {
 		super();
 		this.state = {
 			user: {},
-			users: [],
 			isLoaded: false,
+			noUser: false,
 		};
 		this.getUserRoom = this.getUserRoom.bind(this);
 	}
@@ -24,7 +25,7 @@ class Chat extends React.Component {
 	async componentDidMount() {
 		// before anything, check to see if user object exists in localStorage and get information from Redux store
 		const loggedInUser = window.localStorage.getItem('user');
-		const { nameFromStore, roomFromStore, createUser, getUsersInRoom, getUser } = this.props;
+		const { nameFromStore, roomFromStore, createUser, getUser } = this.props;
 
 		// initialize chatbot to start!
 		clientSocket.on('initializeChatbot', async ({ user: name, room, text: message }) => {
@@ -39,24 +40,49 @@ class Chat extends React.Component {
 
 		// if user is already logged in, on refresh, set the state with the user object from localStorage and fetch users in the room
 		if (loggedInUser) {
-			const user = JSON.parse(loggedInUser);
-			const room = user.room;
 			// once we parsed the loggedInUser, we can use the id to fetch the user from the db and continue as normal
-			const dbUser = await getUser(user.id);
-			// should return the users in the room
-			const users = await getUsersInRoom(room);
-			this.setState({ user: dbUser, users });
-			// initialize chatbot and officially join room after user is created.
-			clientSocket.emit('join', dbUser);
+			try {
+				const user = JSON.parse(loggedInUser);
+				const dbUser = await getUser(user.id);
+
+				if (!dbUser) {
+					// send user back to home if local storage user cannot match with database user
+					this.setState({ noUser: true });
+					throw new Error('failed to fetch user information.');
+				}
+				// if everything went well, set state with user info.
+				this.setState({ user: dbUser });
+
+				// initialize chatbot and officially join room after user is created.
+				clientSocket.emit('join', dbUser);
+			} catch (err) {
+				console.log(err);
+			}
 		} else {
 			// using information from the Redux store, we can create a new user. If user does not exist when we try to getItem, setItem in localStorage and db.
 			// name and room is set in the Home component.
-			const user = await createUser(nameFromStore, roomFromStore);
-			window.localStorage.setItem('user', JSON.stringify({ id: user.id, name: nameFromStore, room: roomFromStore }));
-			this.setState({ user });
-			// initialize chatbot and officially join room after user is created.
-			clientSocket.emit('join', user);
+			try {
+				if (nameFromStore === '') {
+					this.setState({ noUser: true });
+					throw new Error('Name input cannot be empty');
+				}
+
+				const user = await createUser(nameFromStore, roomFromStore);
+				if (!user) {
+					// send user to home if object is deleted from localstorage.
+					this.setState({ noUser: true });
+				}
+				// if user was created properly setItem in local storage and change state.
+				window.localStorage.setItem('user', JSON.stringify({ id: user.id, name: nameFromStore, room: roomFromStore }));
+				this.setState({ user });
+
+				// initialize chatbot and officially join room after user is created.
+				clientSocket.emit('join', user);
+			} catch (err) {
+				console.log(err);
+			}
 		}
+
 		this.setState({ isLoaded: true });
 	}
 
@@ -67,12 +93,17 @@ class Chat extends React.Component {
 	}
 
 	getUserRoom() {
+		// makes sure that we can return the user.room after the page finishes re-rendering
 		if (this.state.isLoaded) {
 			return this.state.user.room;
 		}
 	}
 
 	render() {
+		if (this.state.noUser) {
+			return <Redirect to="/" />;
+		}
+
 		if (!this.state.isLoaded) {
 			return 'Loading...';
 		} else {
--- src/server/api/users.js
+++ src/server/api/users.js
@@ -3,51 +3,56 @@ const router = express.Router();
 const User = require('../db/models/Users');
 
 router.get('/', async (req, res, next) => {
-    try {
-        const users = await User.findAll();
-        if(!users) res.status(404).send('no users in the db');
-        res.send(users);        
-    } catch (err) {
-        next(err);
-    }
-
+	try {
+		const users = await User.findAll();
+		if (!users) res.status(404).send('no users in the db');
+		else res.send(users);
+	} catch (err) {
+		next(err);
+	}
 });
 router.get('/:id', async (req, res, next) => {
-    try {
-        const user = await User.findByPk(req.params.id);
-        if(!user) res.status(404).send('failed to find user');
-        res.send(user);
-    } catch (err) {
-        next(err);
-    }
-})
+	try {
+		const user = await User.findByPk(req.params.id);
+		if (!user) res.status(404).send('failed to find user');
+		else res.send(user);
+	} catch (err) {
+		next(err);
+	}
+});
 
 router.post('/', async (req, res, next) => {
-    try {
-        const user = await User.create({ name: req.body.name, room: req.body.room });
-        if(!user) res.status(404).send('failed to create user');
-        res.send(user);
-    } catch (err) {
-        next(err);
-    }
-})
+	try {
+		const user = await User.create({ name: req.body.name, room: req.body.room });
+		if (!user) res.status(404).send('failed to create user');
+		else res.send(user);
+	} catch (err) {
+		next(err);
+	}
+});
 router.delete('/:id', async (req, res, next) => {
-    try {
-        const user = await User.findByPk(req.params.id);
-        if(!user) res.status(404).send('failed to find user');
-        await user.destroy();
-        res.send(user);
-    } catch (err) {
-        next(err);
-    }
-})
+	try {
+		const user = await User.findByPk(req.params.id);
+		if (!user) {
+			res.status(404).send('failed to find user');
+			throw new Error('failed to find user');
+		} else {
+			await user.destroy();
+			res.send(user);
+		}
+	} catch (err) {
+		next(err);
+	}
+});
 
 router.get('/room/:room', async (req, res, next) => {
-    try {
-        const users = await User.findAll({ where: { room: req.params.room }});
-        res.send(users);
-    } catch (err) {
-        next(err);
-    }
-})
+	try {
+		const users = await User.findAll({ where: { room: req.params.room } });
+		res.send(users);
+	} catch (err) {
+		next(err);
+	}
+});
 module.exports = router;
+
+// NOTE: express handles 404's no need to throw new Error object.
