6ecae6a58564c86de2e9dd86c99ad94c60699e11
super-rogatory <chukwudi10@gmail.com>
2021-10-04 22:47:35 -0700

    junction tables are capitalized in psql

--- server/db/models/Participants.js
+++ server/db/models/Participants.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
-const Participants = db.define('participants', {
+const Participants = db.define('Participants', {
 	id: {
 		type: Sequelize.INTEGER,
 		primaryKey: true,
